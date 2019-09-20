c4e7a520f0fd596e717ff33b3f8bb2ecdb698546
adanl.benito <adanl.benito@gmail.com>
2019-09-20 18:53:37 +0100

    Updated Trill library with methods for touch detection. Added mode to setup function and overriding setup() with threshold and prescaler. Fixed identify() function and added methods for retrieving version and mode.  Included flag for signaling when the device is ready for reading data, this flag is set to false whenever a command has been sent.

--- lib/linux/Trill.h
+++ lib/linux/Trill.h
@@ -1,5 +1,4 @@
 #include <I2c.h>
-#include <stdint.h>
 
 class Trill : public I2c
 {
@@ -21,18 +20,26 @@ class Trill : public I2c
 			kOffsetCommand = 0,
 			kOffsetData = 4
 		};
-		
+
 		enum {
 			kNormalLengthDefault = 20,
+			kNormalLength2D = 32,
 			kRawLength = 60
 		};
-		
-		bool isReady;
-		uint8_t device_type_; // Which type of device is connected (if any)	
+
+		enum {
+			kMaxTouchNum1D = 5,
+			kMaxTouchNum2D = 4
+		};
+
+		bool preparedForDataRead_ = false;
+		uint8_t device_type_; // Which type of device is connected (if any)
 		uint8_t firmware_version_; // Firmware version running on the device
+		uint8_t num_touches_; // Number of touches on last read
+		uint8_t mode_; // Which mode the device is in
 
 		uint8_t dataBuffer[kRawLength];
-		uint16_t commandSleepTime = 10000;;
+		uint16_t commandSleepTime = 10000;
 
 	public:
 		static unsigned int constexpr numSensors = 26;
@@ -45,26 +52,47 @@ class Trill : public I2c
 			DIFF = 3
 		};
 
+		enum Devices {
+			NONE = 0,
+			ONED = 1,
+			TWOD = 2
+		};
+
 		Trill();
 		~Trill();
-		Trill(int i2c_bus, int i2c_address);
-		int setup(int i2c_bus = 1, int i2c_address = 0x18);
+		Trill(int i2c_bus, int i2c_address, int mode);
+		int setup(int i2c_bus = 1, int i2c_address = 0x18, int mode = NORMAL);
+		int setup(int i2c_bus, int i2c_address, int mode, int threshold, int prescaler);
 		void cleanup();
-		
-		bool ready(){ return isReady; }
+
+		bool isReady(){ return preparedForDataRead_; }
 
 		/* Update the baseline value on the sensor*/
 		int updateBaseLine();
 		int prepareForDataRead();
-		int readI2C();
+		int readI2C(); // This should maybe be renamed readRawData()
+		int readLocations();
 		/* Return the type of the device attached or 0 if none is attached */
+		int deviceType() { return device_type_; }
+		int firmwareVersion() { return firmware_version_; }
+		int getMode() { return mode_; }
 		int identify();
-		
-		/* --- Scan configuratin settings --- */
+
+		/* --- Scan configuration settings --- */
 		int setMode(uint8_t mode);
 		int setScanSettings(uint8_t speed, uint8_t num_bits = 12);
 		int setPrescaler(uint8_t prescaler);
 		int setNoiseThreshold(uint8_t threshold);
 		int setIDACValue(uint8_t value);
-		
+		int setMinimumTouchSize(uint16_t size);
+
+		/* --- Touch-related information --- */
+		int numberOfTouches();
+		int numberOfHorizontalTouches();
+		int touchLocation(uint8_t touch_num);
+		int touchSize(uint8_t touch_num);
+		/* Only for 2D sensors */
+		int touchHorizontalLocation(uint8_t touch_num);
+		int touchHorizontalSize(uint8_t touch_num);
+
 };
